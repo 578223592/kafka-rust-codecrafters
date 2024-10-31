759140ff380d47622ebd1a16710bc7be312dccc4
siwuxie <cwl990110@163.com>
2024-11-01 00:58:02 +0800

    refactor: 添加学习链接注释
    - 在代码中添加了学习链接注释，便于后续参考

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
 
+// 学到了：https://app.codecrafters.io/courses/kafka/stages/nv3?repo=e2a5f6bf-3345-4c95-840b-2161d08e62a2
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");

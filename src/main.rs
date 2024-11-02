bba83fb9bf265529ac9592e1c45ce42b8c21cb47
siwuxie <cwl990110@163.com>
2024-11-02 15:33:58 +0800

    Send Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
 
 // 学到了：https://app.codecrafters.io/courses/kafka/stages/nv3?repo=e2a5f6bf-3345-4c95-840b-2161d08e62a2
 fn main() {
@@ -9,11 +9,23 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut new_stream) => {
+                // message_size
+                // Header
+                /*
+                Request Header v0 => request_api_key request_api_version correlation_id
+                request_api_key => INT16
+                request_api_version => INT16
+                correlation_id => INT32
+                                   */
+                // Body
+                let mut responese_message_size: Vec<u8> = vec![0, 0, 0, 0];
+                let write_res = new_stream.write(&responese_message_size);
+                let mut responese_correlation_id: Vec<u8> = vec![0, 0, 0, 7];
+                let write_res = new_stream.write(&responese_correlation_id);
             }
             Err(e) => {
                 println!("error: {}", e);

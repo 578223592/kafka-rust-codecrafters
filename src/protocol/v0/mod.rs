a19d219784688ef54d85bbae5a997b172ec2ae06
siwuxie <cwl990110@163.com>
2024-11-08 00:47:50 +0800

    debug to pass test

--- src/protocol/v0/mod.rs
+++ src/protocol/v0/mod.rs
@@ -19,11 +19,7 @@ impl ResponseWorker for ApiVersionsWorker {
                 // request_api_version => INT16
                 correlation_id: requset_parser.get_correlation_id(),
             },
-            body: ApiVersionBody {
-                error_code: 0,
-                api_keys: vec![],
-                throttle_time_ms: 0,
-            },
+            body: ApiVersionBody::new(),
         };
     }
 
@@ -61,19 +57,30 @@ impl ApiVersionBody {
     fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = self.error_code.to_be_bytes().to_vec();
         // (self.api_keys.len()+1)转成i8
-         ;
 
-        bytes.append(&mut ((self.api_keys.len()+1) as i8).to_be_bytes().to_vec()); // num api key records + 1 ； 不知道为啥，但是大家都是这么写的
-        // 迭代器遍历self.api_keys
+        bytes.append(&mut ((self.api_keys.len() + 1) as i8).to_be_bytes().to_vec()); // num api key records + 1 ； 不知道为啥，但是大家都是这么写的
+                                                                                     // 迭代器遍历self.api_keys
         for api_key in self.api_keys.iter() {
             bytes.append(&mut api_key.to_bytes());
         }
 
         bytes.append(&mut TAG_BUFFER.to_be_bytes().to_vec()); //同样不知道为啥
-        bytes.append(&mut self.throttle_time_ms.to_be_bytes().to_vec()); 
+        bytes.append(&mut self.throttle_time_ms.to_be_bytes().to_vec());
         bytes.append(&mut TAG_BUFFER.to_be_bytes().to_vec()); //同样不知道为啥
 
         return bytes;
+    }
+
+    fn new() -> Self {
+        Self {
+            error_code: 0,
+            api_keys: vec![ApiKey {
+                api_key: 18,
+                min_version: 0,
+                max_version: 4,
+            }],
+            throttle_time_ms: 0,
+        }
     }
 }
 

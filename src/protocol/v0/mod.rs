46438c5a22691b6c32febfb8fd83ca8d7a85142b
siwuxie <cwl990110@163.com>
2024-11-08 00:01:19 +0800

    refactor(protocol): 重构 ApiVersionsWorker 和相关结构
    - 更新 ApiVersionsWorker 结构和实现
    - 添加 ApiKey 结构和实现
    - 修改 Header 结构，更名为 HeaderV0
    - 更新 CommonResponseBuilder 结构和实现
    - 优化代码结构，提高可读性和可维护性

--- src/protocol/v0/mod.rs
+++ src/protocol/v0/mod.rs
@@ -1,42 +1,49 @@
+use std::vec;
+
 use super::ResponseWorker;
 
-
-
-
-
 pub(crate) struct ApiVersionsWorker {
-    message_size: i32, //目前先给0 占位
+    message_size: i32, //It specifies the size of the header and body
     request_api_version: i16,
-    header: Header, //
-    body: ApiVersionBody,  // error_code [api_keys] 
+    header: HeaderV0,
+    body: ApiVersionBody, // error_code [api_keys]
 }
 
 impl ResponseWorker for ApiVersionsWorker {
     fn new(requset_parser: super::v2::RequsetParserV2) -> Self {
         return ApiVersionsWorker {
-            message_size: 0,
+            message_size: -1,
             request_api_version: requset_parser.get_request_api_version(),
-            header: Header {
+            header: HeaderV0 {
                 // request_api_key,
                 // request_api_version => INT16
-                
                 correlation_id: requset_parser.get_correlation_id(),
             },
-            body: ApiVersionBody{
+            body: ApiVersionBody {
                 error_code: 0,
+                api_keys: vec![],
+                throttle_time_ms: 0,
             },
-        }
+        };
     }
 
+    /**
+     * response参考：https://kafka.apache.org/protocol.html#The_Messages_ApiVersions
+     */
     fn build_response(&mut self) -> Vec<u8> {
         //判断
-        if !(self.request_api_version<=4 && self.request_api_version>=0) {
+        if !(self.request_api_version <= 4 && self.request_api_version >= 0) {
             self.body.error_code = 35;
         }
 
-        let mut bytes: Vec<u8> = self.message_size.to_be_bytes().to_vec();
+        // let mut bytes: Vec<u8> = self.message_size.to_be_bytes().to_vec();
+        let mut bytes: Vec<u8> = Vec::new();
         bytes.append(&mut self.header.to_bytes());
         bytes.append(&mut self.body.to_bytes());
+
+        self.message_size = bytes.len() as i32;
+        //头插
+        bytes.splice(0..0, self.message_size.to_be_bytes().to_vec());
         return bytes;
     }
 }
@@ -47,36 +54,54 @@ pub(crate) struct ApiVersionHeader {
 
 struct ApiVersionBody {
     error_code: i16,
-    // api_keys: Vec<ApiVersionApiKey>, //
+    api_keys: Vec<ApiKey>,
+    throttle_time_ms: i32,
 }
 impl ApiVersionBody {
-    fn to_bytes(&self) ->  Vec<u8> {
-        return self.error_code.to_be_bytes().to_vec();
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = self.error_code.to_be_bytes().to_vec();
+        // 迭代器遍历self.api_keys
+        for api_key in self.api_keys.iter() {
+            bytes.append(&mut api_key.to_bytes());
+        }
+        bytes.append(&mut self.throttle_time_ms.to_be_bytes().to_vec());
+
+        return bytes;
     }
 }
 
-
+struct ApiKey {
+    api_key: i16,
+    min_version: i16,
+    max_version: i16,
+}
+impl ApiKey {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut res = self.api_key.to_be_bytes().to_vec();
+        res.append(&mut self.min_version.to_be_bytes().to_vec());
+        res.append(&mut self.max_version.to_be_bytes().to_vec());
+        return res;
+    }
+}
 
 pub(crate) struct CommonResponseBuilder {
     message_size: i32,
-    header: Header,
+    header: HeaderV0,
     body: Body,
 }
 
 impl ResponseWorker for CommonResponseBuilder {
-     fn new(requset_parser: super::v2::RequsetParserV2) -> CommonResponseBuilder {
+    fn new(requset_parser: super::v2::RequsetParserV2) -> CommonResponseBuilder {
         match requset_parser.get_request_api_key() {
             0 => {}
             _ => panic!("Invalid correlation id"),
         }
 
-
         CommonResponseBuilder {
             message_size: 0,
-            header: Header {
+            header: HeaderV0 {
                 // request_api_key,
                 // request_api_version => INT16
-                
                 correlation_id: requset_parser.get_correlation_id(),
             },
             body: Body {},
@@ -91,13 +116,14 @@ impl ResponseWorker for CommonResponseBuilder {
     }
 }
 
-pub(crate) struct Header {
+// headerV0  https://kafka.apache.org/protocol.html#protocol_messages
+pub(crate) struct HeaderV0 {
     // request_api_key: i16,
     // request_api_version: i16,
     correlation_id: i32,
 }
 
-impl Header {
+impl HeaderV0 {
     fn to_bytes(&self) -> Vec<u8> {
         let bytes: Vec<u8> = self.correlation_id.to_be_bytes().to_vec();
         return bytes;
